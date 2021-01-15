742154a977891ac11f7832d0fdae0640a805d842
George Luther <george_l@thinkful.com>
2021-01-15 16:50:12 -0500

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component() {
+  render(){
+    return (
+      <main className='App'>
+        <header>
+          
+        </header>
+      </main>
+    );
+  }
+}
+
+export default App;
