b47c3437aa5977a4d7988f5c2ebd486007c04531
George Luther <george_l@thinkful.com>
2021-01-28 21:50:52 -0500

    landing page styling

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './Landing.css'
+import { Link } from 'react-router-dom'
 
 export default function LandingPage() {
     return (
         <section className="blurb">
             <h2>FIND YOUR GROOVE</h2>
-            <p>Polyperc is a space for creating and transforming rhythmic patterns. Here you can get your ideas out faster than ever before.</p>
-            <button id="start">Let's jam!</button>
+            <p>Polyperc is a space for creating and transforming rhythmic patterns. Get your ideas out faster than ever before!</p>
+            <Link to='/workspace' id="start">Let's Jam!</Link>
         </section>
         
     )
